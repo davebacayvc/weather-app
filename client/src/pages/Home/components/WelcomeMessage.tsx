6dbefcb07e3b0428e17bb67dde3610ced480c3c3
= <spencerbacay@gmail.com>
2023-02-22 19:52:10 +0800

    Create a landing page with conditional statement

---
+++ client/src/pages/Home/components/WelcomeMessage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "../../../components/Button/Button";
+import { APP_NAME } from "../../../constants/constants";
+
+const WelcomeMessage: React.FC = () => {
+  return (
+    <div className="welcome-message">
+      <h1>
+        Welcome to the {APP_NAME} web application. Please login with your Github
+        user to use the application and view the weather in your city.
+      </h1>
+      <Button variant="secondary">Login</Button>
+    </div>
+  );
+};
+
+export default WelcomeMessage;
